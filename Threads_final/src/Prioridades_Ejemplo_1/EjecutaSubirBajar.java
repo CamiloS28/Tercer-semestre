4683b393ea8b50880a7c862798a56e6de19556ef
CamiloS28 <juancasilpe@hotmail.com>
2023-02-13 07:43:15 -0500

    Cambios de clase

--- Threads_final/src/Prioridades_Ejemplo_1/EjecutaSubirBajar.java
+++ Threads_final/src/Prioridades_Ejemplo_1/EjecutaSubirBajar.java
@@ -3,12 +3,15 @@ package Prioridades_Ejemplo_1;
 public class EjecutaSubirBajar {
 
 	public static void main(String[] args) {
-		Thread tM = new SubirBajar("MMM");
-		Thread tm = new SubirBajar("mmm");
+		Thread tM = new SubirBajar("Minima");
+		Thread tm = new SubirBajar("Maxima");
+		Thread k = new SubirBajar("7");
 		tm.setPriority(Thread.MAX_PRIORITY);
 		tM.setPriority(Thread.MIN_PRIORITY);
+		k.setPriority(Thread.NORM_PRIORITY);
 		tm.start();
 		tM.start();
+		k.start();
 	}
 
 }
